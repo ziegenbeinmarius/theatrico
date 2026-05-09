6466142285f7fb726a61e1a3d91e171f51318ba2
Marius Ziegenbein <ziegenbeinmarius@gmail.com>
2026-05-09 11:32:08 +0200

    Improve operator page and add external qrCode page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { JoinPage } from './pages/JoinPage';
 import { ScriptView } from './pages/ScriptView';
 import { OperatorPage } from './pages/OperatorPage';
+import { QRPage } from './pages/QRPage';
 
 export function App() {
   return (
@@ -11,6 +12,7 @@ export function App() {
         <Route path="/join" element={<JoinPage />} />
         <Route path="/join/:code" element={<JoinPage />} />
         <Route path="/script/:code" element={<ScriptView />} />
+        <Route path="/qr/:code" element={<QRPage />} />
         <Route path="*" element={<Navigate to="/join" replace />} />
       </Routes>
     </BrowserRouter>
